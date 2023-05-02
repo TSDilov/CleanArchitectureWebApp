8c5a59aa7c58e6be43ff0cfc7721f0ec0c312000
Tsvetelin91 <cv.dilov@abv.bg>
2023-05-02 13:23:00 +0300

    Implementing ShouldReturnAllLists test

---
+++ tests/CleanTesting.Application.IntegrationTests/TodoLists/Queries/GetTodosTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTesting.Application.IntegrationTests.TodoLists.Queries;
+public class GetTodosTests
+{
+
+}
