e269c5888a00bb5e154493d621d9e40c5ab3ab4f
Tsvetelin91 <cv.dilov@abv.bg>
2023-05-07 15:10:25 +0300

    Test for validation exceptions

---
+++ tests/CleanTesting.Application.IntegrationTests/TodoLists/Commands/CreateTodoListTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitectureWebApp.Application.Common.Exceptions;
+using CleanArchitectureWebApp.Application.TodoLists.Commands.CreateTodoList;
+using FluentAssertions;
+
+namespace CleanTesting.Application.IntegrationTests.TodoLists.Commands;
+using static Testing;
+public class CreateTodoListTests : BaseTestFixture
+{
+    [Test]
+    public void ShouldRequireMinimumFields() 
+    {
+        var command = new CreateTodoListCommand();
+
+        FluentActions.Invoking(() => SendAsync(command))
+            .Should().ThrowAsync<ValidationException>();
+    }
+}
